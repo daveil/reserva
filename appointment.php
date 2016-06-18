00b994412acb40037ec851d13bbfc0db48ccda61
Dave Alaras <arroyo.daveil@gmail.com>
2016-06-18 13:44:11 +0800

    Add patients link on appoint sidebar

--- appointment.php
+++ appointment.php
@@ -29,6 +29,7 @@
   					<li><a href="index.php">Home</a></li>
   					<li class="active"><a href="appointment.php">Appointment</a></li>
   					<li><a href="calendar.php">Calendar</a></li>
+            <li><a href="patients.php">Patients</a></li>
   					<li><a href="services-offered.php">Services Offered</a></li>
   					<li><a href="contact-information.php">Contact Information</a></li>
   					<li><a href="about-us.php">About Us</a></li>
