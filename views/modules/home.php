e227cd4fef65a94467a762f17e3931bc9d090f5e
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-07-11 10:29:18 +0800

    Implement templating

---
+++ views/modules/home.php
@@ -0,0 +1,12 @@
+<h2 style="margin-top:0;">Welcome home <?php echo 'User';?>!</h2>
+	<p>Lorem ipsum raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica.</p>
+<div class="row">
+	<div class="col-md-6">
+		<img class="thumbnail" src="bower_components/Bootflat/img/thumbnail-4.jpg" alt=""/>
+	</div>
+	<div class="col-md-6">
+		<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id ...</p>
+		<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id ...</p>
+		<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id ...</p>
+	</div>
+</div>
