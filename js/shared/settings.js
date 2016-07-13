2cf4e865a1f12ba2abbe48e1ed26d928dd98a35b
Dave Alaras <arroyo.daveil@gmail.com>
2016-07-13 20:54:52 +0800

    Fix localhost api url

--- js/shared/settings.js
+++ js/shared/settings.js
@@ -3,7 +3,7 @@
 			DEMO_MODE:false,
 			API_EXT:'json',
 			TEST_URL:'js/tests/',
-			API_URL:window.location.host=='localhost'?'http://localhost/reserva/api/':'http://rsrvsys.esy.es/api/'
+			API_URL:window.location.hostname=='localhost'?window.location.origin+'/reserva/api/':'http://rsrvsys.esy.es/api/'
 			};
 	APP.constant('settings',SETTINGS);	
 })();
