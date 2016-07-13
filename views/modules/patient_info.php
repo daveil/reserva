82d1f6f314ffbe17c7d82a36255ff6cd0aeb3105
Dave Alaras <arroyo.daveil@gmail.com>
2016-07-13 23:00:31 +0800

    Route and display patient_info

--- views/modules/patient_info.php
+++ views/modules/patient_info.php
@@ -1,39 +1,37 @@
 	<h2 style="margin-top:0;">Patient Information</h2>
-	<div class="row">
-	<div class="col-md-4">
-	 <dl>
-	   <dt>Name</dt>
-	   <dd>Juan Dela Cruz</dd>
-	   <dt>Age</dt>
-	   <dd>12</dd>
-	   <dt>Address</dt>
-	   <dd>Santo Tomas, Batangas</dd>
-	 </dl>
-	</div>
-	<div class="col-md-8">
-	  <dl>
-	   <dt>Contact No</dt>
-	   <dd>(043) 211 1111</dd>
-	 </dl>
-	</div>
-	</div>
-	<h3>History</h3>
-	<table class="table">
-	<thead>
-	  <tr>
-		<th>Date</th>
-		<th>Concern</th>
-	  </tr>
-	</thead>
-	<tbody>
-	  <tr>
-		<td>Jun 12,2016</td>
-		<td>Tooth Ache</td>
-	  </tr>
-	  <tr class="active">
-		<td>Jun 13,2016</td>
-		<td>Tooth Ache</td>
-	  </tr>
-	</tbody>
-	</table>
-        
+	<div ng-controller="PatientInfoController" ng-init="PatientId=<?php echo $params[0];?>">
+		<div class="row" >
+			<div class="col-md-4">
+			 <dl>
+			   <dt>Name</dt>
+			   <dd>{{Patient.name}}</dd>
+			   <dt>Age</dt>
+			   <dd>{{Patient.name}}</dd>
+			   <dt>Address</dt>
+			   <dd>{{Patient.address}}</dd>
+			 </dl>
+			</div>
+			<div class="col-md-8">
+			  <dl>
+			   <dt>Contact No</dt>
+			   <dd>{{Patient.contact_no}}</dd>
+			 </dl>
+			</div>
+		</div>
+		<h3>History</h3>
+		<table class="table">
+		<thead>
+		  <tr>
+			<th>Date</th>
+			<th>Concern</th>
+		  </tr>
+		</thead>
+		<tbody>
+		  <tr ng-repeat="Appointment in Appointments">
+			<td>{{Appointment.schedule}}</td>
+			<td>{{Appointment.concern}}</td>
+		  </tr>
+		</tbody>
+		</table>
+     </div>   
+        <?php echo Assest::js('patient_info');?>
