8ca8f3db347f86e4b9cf6626510ad640b2869361
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-07-15 20:58:37 +0800

    Adjust appointment SelectedDate

--- js/appointment.js
+++ js/appointment.js
@@ -1,16 +1,18 @@
 APP.controller('AppointmentController',['$scope','dateFilter','api',function($scope,dateFilter,api){
+	resetAppointment();
 	$scope.minDate = dateFilter(new Date(),'yyyy-MM-dd');
 	$scope.disabledDates = ['2016-07-15','2016-07-20'];
-	resetAppointment();
 	function resetAppointment(){
 		$scope.Patient = {};
 		$scope.Appointment = {};
-		$scope.Appointment.schedule = dateFilter(new Date(),'yyyy-MM-dd');
 		$scope.SavingAppointment = false;
 	}
 	$scope.cancelAppointment = function(){
 		resetAppointment();
 	}
+	$scope.$watch('SelectedDate',function(value){
+		$scope.Appointment.schedule = value;
+	});
 	$scope.bookAppointment = function(){
 		var data = {
 				Patient:$scope.Patient,
