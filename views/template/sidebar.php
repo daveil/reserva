2dd336a3c3139de8c2f1934eceee404665ef0e7b
Dave Alaras <arroyo.daveil@gmail.com>
2016-07-20 23:03:54 +0800

    Add submenu on sidebar

--- views/template/sidebar.php
+++ views/template/sidebar.php
@@ -4,17 +4,45 @@
 				'appointment'=>'Appointment',
 				'calendar'=>'Calendar',
 				'patients'=>'Patients',
-				'services-offered'=>'Services Offered',
+				'services'=>
+					array(
+						'Services Offered',
+						array(
+							'ecg'=>'ECG',
+							'cbc'=>'CBC',
+							'o2-sat-monitoring'=>'O2 Sat. Monitoring',
+							'medical-consultation'=>'Medical Consultation'
+						)
+				),
 				'contact-info'=>'Contact Information',
 				'about-us'=>'About Us',
+				'settings'=>'Settings'
 	);
 ?>
 <ul class="nav nav-pills nav-stacked">
-	<?php foreach($menus as $_url=>$title):?>
-		<li <?php if($url==$_url) echo 'class=active'; ?>>
+	<?php foreach($menus as $_url=>$title):
+				$submenu = null;
+				if(is_array($title)){
+					$submenu =  $title[1];
+					$title =  $title[0];
+				}
+		?>
+		<li class="<?php if($url==$_url) echo 'active'; ?>">
+
 			<a href="<?php echo WEB_URL.DS.$_url;?>">
 				<?php echo $title;?>
 			</a>
+			<?php if($submenu):?>
+				<ul class="nav nav-pills nav-stacked">
+					<?php foreach($submenu as $_suburl=>$subtitle):?>
+					<li class="<?php if($url==$_url.DS.$_suburl) echo 'active'; ?>">
+						<a href="<?php echo WEB_URL.DS.$_url.DS.$_suburl;?>">
+							<?php echo $subtitle;?>
+						</a>
+					</li>
+					<?php endforeach;?>
+				</ul>
+			<?php endif;?>
 		</li>
 	<?php endforeach;?>
 </ul>
