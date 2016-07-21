eebf2286f135ec153f95e2091a390ed1ec91bf3d
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-07-21 15:54:23 +0800

    Return max daily booking

--- api/models/setting.php
+++ api/models/setting.php
@@ -1,4 +1,19 @@
 <?php
 class Setting extends AppModel {
 	var $name = 'Setting';
+	function getRefNo(){
+		$setting = $this->findById('REF_NO_COUNTER');
+		$refNo = (int)$setting['Setting']['value'];
+		return $refNo;
+	}
+	function setRefNo($value){
+		$setting = array();
+		$setting['Setting']=array('id'=>'REF_NO_COUNTER','value'=>(int)$value);
+		return $this->save($setting);
+	}
+	function getMaxDailyBooking(){
+		$setting = $this->findById('MAX_DAILY_BOOKING');
+		$max = (int)$setting['Setting']['value'];
+		return $max;
+	}
 }
