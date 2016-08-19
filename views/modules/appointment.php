4d1b8fb602d8381ca559f36f5583a2a8d76c996f
Dave Arroyo <arroyo.daveil@gmail.com>
2016-08-19 10:53:08 +0800

    Add visual indicator for invalid inputs

--- views/modules/appointment.php
+++ views/modules/appointment.php
@@ -1,7 +1,12 @@
+<style type="text/css"> 
+.form-vertical .ng-invalid.ng-touched {
+    background-color: #FA787E;
+  }
+</style>
 <h3 style="margin-top:0;">Set an appointment</h3>
 <div ng-controller="AppointmentController">
 	{{ShowModal}}
-	<form class="form-vertical">
+	<form class="form-vertical" name="AppointmentForm">
 		<div class="row">
 			<div class="col-md-6">
 			 <div pickadate="" ng-model="SelectedDate" min-date="minDate" disabled-dates="disabledDates" allowed-days="clinicDays" on-change-month="onChangeMonth" id="SelectedDate"></div>
@@ -12,24 +17,24 @@
 					<input class="form-control" ng-model="Appointment.schedule"  type="text" disabled>
 				</div>
 				<div class="form-group col-md-12">
-					<label for="">Name</label>
-					<input class="form-control"  type="text" ng-model="Patient.name">
+					<label for="">Name</label>			
+					<input class="form-control"  type="text" name="name" ng-model="Patient.name" required />
 				</div>
 				<div class="form-group col-md-6">
 				  <label for="">Contact No. </label>
-				  <input type="text" class="form-control"  ng-model="Patient.contact_no"/>
+				  <input type="text" class="form-control" name="contact_no" ng-model="Patient.contact_no"  ng-minlength="11" required/>
 				</div>
 				<div class="form-group col-md-6">
 					<label for="">Age</label>
-					<input class="form-control"  type="number" ng-model="Patient.age">
+					<input class="form-control"  name="age" type="number" ng-model="Patient.age" required/>
 				</div>
 				<div class="form-group col-md-12">
 					<label for="">Address</label>
-					<input class="form-control" type="text" ng-model="Patient.adddress">
+					<input class="form-control" name="address" type="text"  ng-model="Patient.adddress" required/>
 				</div>
 				<div class="form-group col-md-12">
 					<label for="">Concern</label>
-					<textarea class="form-control" name="" id="" cols="30" rows="3" ng-model="Appointment.concern"></textarea>
+					<textarea class="form-control" name="concern" id="" cols="30" rows="3" ng-model="Appointment.concern" required></textarea>
 				</div>
 			</div>
 		</div>
