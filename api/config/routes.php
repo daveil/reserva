bc777e6833f9b612e15aba22099f1805688726ff
Dave Arroyo <arroyo.daveil@gmail.com>
2016-08-20 19:21:30 +0800

    Add login to routes

--- api/config/routes.php
+++ api/config/routes.php
@@ -33,4 +33,5 @@
  * ...and connect the rest of 'Pages' controller's urls.
  */
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+	Router::connect('/login', array('controller' => 'users', 'action' => 'login'));
 	Router::connect('/register', array('controller' => 'users', 'action' => 'add'));
