57a35d8b18d3b5ec48faa1a5b010ca99d47b5442
Dave Arroyo <arroyo.daveil@gmail.com>
2016-08-20 21:20:17 +0800

    Upload to phpmyadmin

---
+++ users.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Ultimate v9.10 
+MySQL - 5.5.5-10.1.13-MariaDB : Database - reserva
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(10) DEFAULT 'patient',
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `contact_no` char(11) DEFAULT NULL,
+  `address` varchar(150) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`type`,`username`,`password`,`name`,`age`,`contact_no`,`address`,`created`,`modified`) values (1,'admin','admin','5f4dcc3b5aa765d61d8327deb882cf99',NULL,NULL,NULL,NULL,'2016-08-20 21:11:59','2016-08-20 21:11:59'),(2,'patient','patient','5f4dcc3b5aa765d61d8327deb882cf99',NULL,NULL,NULL,NULL,'2016-08-20 21:12:13','2016-08-20 21:12:13');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
