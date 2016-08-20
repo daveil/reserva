b27825d810666dfa846139f5cd62248b2055b03e
Dave Arroyo <arroyo.daveil@gmail.com>
2016-08-20 18:36:36 +0800

    Add login and register api

--- js/login.js
+++ js/login.js
@@ -1,6 +1,28 @@
 APP.controller('LoginController',['$scope','api',function($scope,api){
-
+	$scope.cancel = function(){
+		$scope.Username = null;
+		$scope.Password = null;
+	}
+	$scope.login = function(){
+		var data = {};
+			data.username =  $scope.Username;
+			data.password =  $scope.Password;
+		api.POST('login',data,function(response){
+			
+		});
+	}
 }]);
 APP.controller('RegisterController',['$scope','api',function($scope,api){
-
+	$scope.cancel = function(){
+		$scope.Username = null;
+		$scope.Password = null;
+	}
+	$scope.register = function(){
+		var data = {};
+			data.username =  $scope.Username;
+			data.password =  $scope.ConfirmPassword;
+		api.POST('register',data,function(response){
+			
+		});
+	}
 }]);
