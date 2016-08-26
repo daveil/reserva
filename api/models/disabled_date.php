8d2ab8d62145476151c42db4aa05dd76acc1bada
Dave Arroyo <arroyo.daveil@gmail.com>
2016-08-26 09:05:25 +0800

    Add getStatus function

--- api/models/disabled_date.php
+++ api/models/disabled_date.php
@@ -4,12 +4,25 @@ class DisabledDate extends AppModel {
 	function getDates($date){
 		$month = date("m",strtotime($date));
 		$fields = array('DisabledDate.date');
-		$conditions = array('MONTH(DisabledDate.date)'=>$month );
+		$statuses = array('full','disabled');
+		$conditions = array(
+				'MONTH(DisabledDate.date)'=>$month,
+				'DisabledDate.status'=>$statuses
+				);
 		$results = $this->find('all',compact('fields','conditions'));
 		$dates = array();
 		foreach($results as $r){
 			array_push($dates,$r['DisabledDate']['date']);
 		}
 		return $dates;
+	}
+	function getStatus($schedule){
+		$statuses = array('full','disabled');
+		$conditions = array(
+				'DisabledDate.date'=>$schedule,
+				'DisabledDate.status'=>$statuses
+				);
+		$count = $this->find('count',compact('conditions'));
+		return $count==0;
 	}
 }
