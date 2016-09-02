7275411fc729b13c6cd73f9c79a901b04d103dce
Dave Arroyo <arroyo.daveil@gmail.com>
2016-09-03 07:15:59 +0800

    Edit profile info and add back to patients link

--- js/patient_info.js
+++ js/patient_info.js
@@ -2,8 +2,26 @@ APP.controller('PatientInfoController',['$scope','api',function($scope,api){
 	$scope.$watch('PatientId',function(value){
 		api.GET('patients/view/'+value).then(function(response){
 			$scope.Patient = response.data.Patient;
+			$scope.PatientCopy = response.data.Patient;
 			$scope.Appointments = response.data.Appointment;
 		});	
 	});
+	$scope.Mode = 'VIEW';
+	$scope.edit = function(){
+		$scope.Mode ='EDIT';
+	}
+	$scope.cancel = function(){
+		$scope.Mode = 'VIEW';
+		$scope.Patient = angular.copy($scope.PatientCopy);
+	}
+	$scope.updateInfo = function(){
+		var data = $scope.Patient;
+		$scope.Saving = true;
+		api.POST('users/edit/profile',data).then(function(response){
+			$scope.Saving = false;
+			$scope.PatientCopy =  angular.copy($scope.Patient);
+			alert(response.data.message);
+		});
+	}
 	
 }]);
