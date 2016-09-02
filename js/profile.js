5bf5c0d406cb847f44d965dd5af96d0974cb008a
Dave Arroyo <arroyo.daveil@gmail.com>
2016-09-03 05:02:00 +0800

    Initialize profile module

---
+++ js/profile.js
@@ -0,0 +1,3 @@
+APP.controller('ProfileController',['$scope','api',function($scope,api){
+	
+}]);
---
+++ views/modules/profile.php
@@ -0,0 +1,4 @@
+<h3 style="margin-top:0;">Profile</h3>
+<div ng-controller="ProfileController">
+</div>
+<?php echo Assest::js('profile');?>
